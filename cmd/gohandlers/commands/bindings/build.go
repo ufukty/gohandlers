ddbf924929eb055ceffdcc28af74e95d03ba125d
Ufuk <13807261+ufukty@users.noreply.github.com>
2024-11-07 12:47:12 +0300

    bindings: improves file organization

--- cmd/gohandlers/commands/bindings/build.go
+++ cmd/gohandlers/commands/bindings/build.go
@@ -204,61 +204,6 @@ func build(info inspects.Info) *ast.FuncDecl {
 	return fd
 }
 
-func pathvarimports(infoss map[inspects.Receiver]map[string]inspects.Info) bool {
-	for _, infos := range infoss {
-		for _, info := range infos {
-			if info.RequestType != nil && len(info.RequestType.RouteParams) > 0 {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func bodyimports(infoss map[inspects.Receiver]map[string]inspects.Info) bool {
-	for _, infos := range infoss {
-		for _, info := range infos {
-			if info.RequestType != nil && info.RequestType.ContainsBody {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func imports(infoss map[inspects.Receiver]map[string]inspects.Info) []ast.Spec {
-	imports := []ast.Spec{
-		&ast.ImportSpec{Path: &ast.BasicLit{Kind: token.STRING, Value: `"fmt"`}},
-		&ast.ImportSpec{Path: &ast.BasicLit{Kind: token.STRING, Value: `"logbook/internal/utils/urls"`}},
-		&ast.ImportSpec{Path: &ast.BasicLit{Kind: token.STRING, Value: `"logbook/internal/web/balancer"`}},
-		&ast.ImportSpec{Path: &ast.BasicLit{Kind: token.STRING, Value: `"net/http"`}},
-	}
-	if bodyimports(infoss) {
-		imports = append(imports,
-			&ast.ImportSpec{Path: &ast.BasicLit{Kind: token.STRING, Value: `"bytes"`}},
-			&ast.ImportSpec{Path: &ast.BasicLit{Kind: token.STRING, Value: `"encoding/json"`}},
-			&ast.ImportSpec{Path: &ast.BasicLit{Kind: token.STRING, Value: `"mime"`}},
-		)
-	}
-	if pathvarimports(infoss) {
-		imports = append(imports,
-			&ast.ImportSpec{Path: &ast.BasicLit{Kind: token.STRING, Value: `"strings"`}},
-		)
-	}
-	slices.SortFunc(imports, func(a, b ast.Spec) int {
-		av := a.(*ast.ImportSpec).Path.Value
-		bv := b.(*ast.ImportSpec).Path.Value
-		if av < bv {
-			return -1
-		} else if av == bv {
-			return 0
-		} else {
-			return 1
-		}
-	})
-	return imports
-}
-
 func buildfuncs(infoss map[inspects.Receiver]map[string]inspects.Info) []ast.Decl {
 	fds := []ast.Decl{}
 	for _, infos := range infoss {
