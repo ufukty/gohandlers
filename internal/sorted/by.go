6663e2e2da573797291e5a1af97c0bc38eedaa76
Ufuk <13807261+ufukty@users.noreply.github.com>
2025-01-27 14:08:32 +0300

    int/sorted: improves clarity

--- internal/sorted/by.go
+++ internal/sorted/by.go
@@ -11,15 +11,7 @@ import (
 func ByValues[K comparable, V cmp.Ordered](m map[K]V) iter.Seq2[K, V] {
 	o := maps.Keys(m)
 	slices.SortFunc(o, func(a, b K) int {
-		va := m[a]
-		vb := m[b]
-		if va < vb {
-			return -1
-		} else if va > vb {
-			return 1
-		} else {
-			return 0
-		}
+		return cmp.Compare(m[a], m[b])
 	})
 	return func(yield func(K, V) bool) {
 		for _, k := range o {
