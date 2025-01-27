aba36863c0cf29b9772c9ae8fedfdc4e99a4c6f5
Ufuk <13807261+ufukty@users.noreply.github.com>
2025-01-27 18:29:44 +0300

    cmd/gohandlers/commands/bindings/produce: improves declaration order

--- cmd/gohandlers/commands/bindings/produce/bqparse.go
+++ cmd/gohandlers/commands/bindings/produce/bqparse.go
@@ -15,70 +15,6 @@ type bqParse struct {
 	table bqParseSymbolTable
 }
 
-func (p *bqParse) Produce(info inspects.Info) *ast.FuncDecl {
-	fd := &ast.FuncDecl{
-		Recv: &ast.FieldList{List: []*ast.Field{
-			{Names: []*ast.Ident{{Name: "bq"}}, Type: &ast.StarExpr{X: &ast.Ident{Name: info.RequestType.Typename}}},
-		}},
-		Name: &ast.Ident{Name: "Parse"},
-		Type: &ast.FuncType{
-			Params: &ast.FieldList{List: []*ast.Field{{
-				Names: []*ast.Ident{{Name: "rq"}},
-				Type:  &ast.StarExpr{X: &ast.SelectorExpr{X: &ast.Ident{Name: "http"}, Sel: &ast.Ident{Name: "Request"}}},
-			}}},
-			Results: &ast.FieldList{List: []*ast.Field{
-				{Type: &ast.Ident{Name: "error"}},
-			}},
-		},
-		Body: &ast.BlockStmt{List: []ast.Stmt{}},
-	}
-
-	fd.Body.List = append(fd.Body.List, p.route(info)...)
-	fd.Body.List = append(fd.Body.List, p.query(info)...)
-
-	if info.RequestType.ContainsBody {
-		fd.Body.List = append(fd.Body.List,
-			&ast.AssignStmt{
-				Lhs: []ast.Expr{&ast.Ident{Name: "err"}},
-				Tok: ternary(p.table.err, token.ASSIGN, token.DEFINE),
-				Rhs: []ast.Expr{
-					&ast.CallExpr{
-						Fun: &ast.SelectorExpr{
-							X: &ast.CallExpr{
-								Fun:  &ast.SelectorExpr{X: &ast.Ident{Name: "json"}, Sel: &ast.Ident{Name: "NewDecoder"}},
-								Args: []ast.Expr{&ast.SelectorExpr{X: &ast.Ident{Name: "rq"}, Sel: &ast.Ident{Name: "Body"}}},
-							},
-							Sel: &ast.Ident{Name: "Decode"},
-						},
-						Args: []ast.Expr{&ast.Ident{Name: "bq"}},
-					},
-				},
-			},
-			&ast.IfStmt{
-				Cond: &ast.BinaryExpr{X: &ast.Ident{Name: "err"}, Op: token.NEQ, Y: &ast.Ident{Name: "nil"}},
-				Body: &ast.BlockStmt{List: []ast.Stmt{
-					&ast.ReturnStmt{Results: []ast.Expr{
-						&ast.CallExpr{
-							Fun: &ast.SelectorExpr{X: &ast.Ident{Name: "fmt"}, Sel: &ast.Ident{Name: "Errorf"}},
-							Args: []ast.Expr{
-								&ast.BasicLit{Kind: token.STRING, Value: `"decoding body: %w"`},
-								&ast.Ident{Name: "err"},
-							},
-						},
-					}},
-				}},
-			},
-		)
-		p.table.err = true
-	}
-
-	fd.Body.List = append(fd.Body.List,
-		&ast.ReturnStmt{Results: []ast.Expr{&ast.Ident{Name: "nil"}}},
-	)
-
-	return fd
-}
-
 func (p *bqParse) query(info inspects.Info) []ast.Stmt {
 	stmts := []ast.Stmt{}
 	if len(info.RequestType.Params.Query) > 0 {
@@ -173,6 +109,70 @@ func (p *bqParse) route(info inspects.Info) []ast.Stmt {
 		p.table.err = true
 	}
 	return stmts
+}
+
+func (p *bqParse) Produce(info inspects.Info) *ast.FuncDecl {
+	fd := &ast.FuncDecl{
+		Recv: &ast.FieldList{List: []*ast.Field{
+			{Names: []*ast.Ident{{Name: "bq"}}, Type: &ast.StarExpr{X: &ast.Ident{Name: info.RequestType.Typename}}},
+		}},
+		Name: &ast.Ident{Name: "Parse"},
+		Type: &ast.FuncType{
+			Params: &ast.FieldList{List: []*ast.Field{{
+				Names: []*ast.Ident{{Name: "rq"}},
+				Type:  &ast.StarExpr{X: &ast.SelectorExpr{X: &ast.Ident{Name: "http"}, Sel: &ast.Ident{Name: "Request"}}},
+			}}},
+			Results: &ast.FieldList{List: []*ast.Field{
+				{Type: &ast.Ident{Name: "error"}},
+			}},
+		},
+		Body: &ast.BlockStmt{List: []ast.Stmt{}},
+	}
+
+	fd.Body.List = append(fd.Body.List, p.route(info)...)
+	fd.Body.List = append(fd.Body.List, p.query(info)...)
+
+	if info.RequestType.ContainsBody {
+		fd.Body.List = append(fd.Body.List,
+			&ast.AssignStmt{
+				Lhs: []ast.Expr{&ast.Ident{Name: "err"}},
+				Tok: ternary(p.table.err, token.ASSIGN, token.DEFINE),
+				Rhs: []ast.Expr{
+					&ast.CallExpr{
+						Fun: &ast.SelectorExpr{
+							X: &ast.CallExpr{
+								Fun:  &ast.SelectorExpr{X: &ast.Ident{Name: "json"}, Sel: &ast.Ident{Name: "NewDecoder"}},
+								Args: []ast.Expr{&ast.SelectorExpr{X: &ast.Ident{Name: "rq"}, Sel: &ast.Ident{Name: "Body"}}},
+							},
+							Sel: &ast.Ident{Name: "Decode"},
+						},
+						Args: []ast.Expr{&ast.Ident{Name: "bq"}},
+					},
+				},
+			},
+			&ast.IfStmt{
+				Cond: &ast.BinaryExpr{X: &ast.Ident{Name: "err"}, Op: token.NEQ, Y: &ast.Ident{Name: "nil"}},
+				Body: &ast.BlockStmt{List: []ast.Stmt{
+					&ast.ReturnStmt{Results: []ast.Expr{
+						&ast.CallExpr{
+							Fun: &ast.SelectorExpr{X: &ast.Ident{Name: "fmt"}, Sel: &ast.Ident{Name: "Errorf"}},
+							Args: []ast.Expr{
+								&ast.BasicLit{Kind: token.STRING, Value: `"decoding body: %w"`},
+								&ast.Ident{Name: "err"},
+							},
+						},
+					}},
+				}},
+			},
+		)
+		p.table.err = true
+	}
+
+	fd.Body.List = append(fd.Body.List,
+		&ast.ReturnStmt{Results: []ast.Expr{&ast.Ident{Name: "nil"}}},
+	)
+
+	return fd
 }
 
 func BqParse(i inspects.Info) *ast.FuncDecl {
