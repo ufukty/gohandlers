028c6b084f9a6c2c0e7bff2c4c2b2095c1864f59
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-04-24 17:36:23 +0300

    int/sorted: replaces x/exp/maps provided Keys with stdlib equivalent

--- internal/sorted/by.go
+++ internal/sorted/by.go
@@ -3,18 +3,16 @@ package sorted
 import (
 	"cmp"
 	"iter"
+	"maps"
 	"slices"
-
-	"golang.org/x/exp/maps"
 )
 
 func ByValues[K comparable, V cmp.Ordered](m map[K]V) iter.Seq2[K, V] {
-	o := maps.Keys(m)
-	slices.SortFunc(o, func(a, b K) int {
+	sorted := slices.SortedFunc(maps.Keys(m), func(a, b K) int {
 		return cmp.Compare(m[a], m[b])
 	})
 	return func(yield func(K, V) bool) {
-		for _, k := range o {
+		for _, k := range sorted {
 			if !yield(k, m[k]) {
 				return
 			}
