112322986825f228a197a0aaba36b3a6a0154f93
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-07-01 15:28:40 +0300

    cmd/gohandlers/internal/pretty: adjusts whitespaces

--- cmd/gohandlers/internal/pretty/post/process.go
+++ cmd/gohandlers/internal/pretty/post/process.go
@@ -5,13 +5,17 @@ import (
 	"strings"
 )
 
+var typedecls = regexp.MustCompile(`(?m)$(\n(?://.*\n)*type)`)
+var funcdecls = regexp.MustCompile(`(?m)$(\n(?://.*\n)*func)`)
 var imports = regexp.MustCompile(`(?m)import \(((?:\s+"[\w]+(?:/[\w.]+)*"\n)*)((?:\s+"[\w.]+(?:/[\w.]+)*"\n)*)\)`)
 
-func Process(f string) string {
-	f = strings.ReplaceAll(f, "}\nfunc", "}\n\nfunc")
-	f = strings.ReplaceAll(f, "HandlerInfo{", "HandlerInfo{\n") // beginning composite literal
-	f = strings.ReplaceAll(f, "}, \"", "},\n\"")                // after each line
-	f = strings.ReplaceAll(f, "}}", "},\n}")                    // ending composite literal
-	f = imports.ReplaceAllString(f, "import ($1\n$2)")          // split packages starts with domains
-	return f
+func Process(s string) string {
+	s = typedecls.ReplaceAllString(s, "\n$1")
+	s = funcdecls.ReplaceAllString(s, "\n$1")
+	s = imports.ReplaceAllString(s, "import ($1\n$2)") // split packages starts with domains
+	// 3 lines are for iterators
+	s = strings.ReplaceAll(s, "{\"", "{\n\"")
+	s = strings.ReplaceAll(s, ", \"", ",\n\"")
+	s = strings.ReplaceAll(s, "}\n\t\tfor", ",\n}\n\t\tfor")
+	return s
 }
