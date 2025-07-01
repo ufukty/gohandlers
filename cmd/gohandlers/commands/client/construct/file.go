743d81bdf839562067fee91970028459f6e21f09
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2025-07-01 13:11:03 +0300

    cmd/gohandlers/commands/client/construct: improves readability

--- cmd/gohandlers/commands/client/construct/file.go
+++ cmd/gohandlers/commands/client/construct/file.go
@@ -97,16 +97,14 @@ func clientConstructor() ast.Decl {
 
 func File(infoss map[inspects.Receiver]map[string]inspects.Info, pkgdst, pkgsrc, importpkg string) *ast.File {
 	f := &ast.File{
-		Name:  &ast.Ident{Name: pkgdst},
-		Decls: []ast.Decl{},
+		Name: &ast.Ident{Name: pkgdst},
+		Decls: []ast.Decl{
+			imports(importpkg),
+			pool(),
+			client(),
+			clientConstructor(),
+		},
 	}
-
-	f.Decls = append(f.Decls,
-		imports(importpkg),
-		pool(),
-		client(),
-		clientConstructor(),
-	)
 
 	fds := []ast.Decl{}
 	for _, infos := range infoss {
