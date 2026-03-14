27c517bec950d19021c582a7583570f79a61d5c0
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-03-14 12:17:00 +0300

    dev/cf/pages/build-docs.sh: changes the build script use content at latest versioned commit when it is triggered on the `main` branch. changes to use fixed version of Kask. changes to prepend links in pages with the new domain.

--- dev/cloudflare/pages/build-docs.sh
+++ dev/cloudflare/pages/build-docs.sh
@@ -4,11 +4,14 @@ set -ve
 
 (
   cd "$(mktemp -d)"
-  git clone https://github.com/ufukty/kask
-  cd kask
-  git fetch --tags --quiet
-  git checkout "$(git tag --list 'v*' | sort -Vr | head -n 1)"
+  git clone --branch v0.16.1 --depth 1 https://github.com/ufukty/kask .
   make install
 )
 
-~/bin/kask build -in docs -out docs-build -domain / -v
+git fetch --tags --quiet
+
+if test "$WORKERS_CI_BRANCH" = "main"; then
+  git checkout "$(git tag --list 'v*' --sort '-version:refname' | head -n 1)"
+fi
+
+~/bin/kask build -in docs -out docs-build -domain "https://gohandlers.ufukty.com" -v -cfw
